d4e6bbc31ea4bb9b268fd369c0ee90b67143c9c8
Ilmarden Júnior <irljbsb@gmail.com>
2023-02-09 17:09:37 -0300

    Primeiro Git

---
+++ script.js
@@ -0,0 +1,108 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+     });
+
+     var typed = new Typed(".typing", {
+         strings:["Analista de Sistemas", "Desenvolvedor", "Designer", "Freelancer"],
+         typeSpeed:100,
+         backSpeed:60,
+         loop:true
+     });
+     var typed = new Typed(".typing-2", {
+        strings:["Analista de Sistemas", "Desenvolvedor", "Designer", "Freelancer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    });
+});
+
+
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight *3) / 4);
+    target.forEach(function(element){
+        if((windowTop) > element.offsetTop){
+            element.classList.add(animationClass);
+        }else{
+            element.classList.remove(animationClass);
+        }
+    })
+}
+
+animeScroll();
+
+if(target.length){
+    window.addEventListener('scroll', debounce(function(){
+    animeScroll(); 
+    console.log('jr');
+    }, 200));
+}
+
+
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference;
+}
+
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
